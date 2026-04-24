fd8a0112d166dccc768dfc96fe22e0236bff48c7
Lakshmi-ctr <laxmipnlaxmipn@gmail.com>
2026-04-24 12:28:14 +0530

    My First commit

--- src/com/java/FirstPragrams.java
+++ src/com/java/FirstPragrams.java
@@ -3,5 +3,6 @@ package com.java;
 public class FirstPragrams {
 	void print() {
 		System.out.println("Welcome to the  github the session");	
+		System.out.println("Welcome to the  github the session");	
 		}
 }
